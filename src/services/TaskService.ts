5302bded3bbc673062bb5911ea0ece5c2a2f6095
Emiliano_Moreno <Emimoreno170793@gmail.com>
2023-11-22 19:50:20 -0300

    agregando url de render

--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -1,7 +1,7 @@
 import { Task } from "../types/Task";
 
 
-const BASE_URL = 'http://localhost:3000/tasks';
+const BASE_URL = 'https://trabajo-final-arg-programa-back.onrender.com/tasks';
 
 export const TaskService = {
     getAllTasks: async (): Promise<Task[]> => {
